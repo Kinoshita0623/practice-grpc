5b27b24fc91cdbba25b105570ad78d1daeaff95e
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-08-02 16:25:38 +0900

    [change]

---
+++ app/sum.go
@@ -0,0 +1,5 @@
+package app
+
+func Sum(a, b int) int {
+	return a + b
+}
--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"com.github.Kinoshita0623/practice-grpc/app"
+	"com.github.Kinoshita0623/practice-grpc/app/service"
 )
 
 func main() {
 
-	fmt.Printf("hello")
+	fmt.Printf("hello\n")
+	result := app.Sum(1, 2)
+	fmt.Printf("sum:%d\n", result)
+	service.Test()
 }
