b9632ebf99451abc6932f563d4087ea9621a8e54
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-08-02 16:36:11 +0900

    [change]

--- client/main.go
+++ client/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	pb "com.github.Kinoshita0623/practice-grpc/pb/cat"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("127.0.0.1:19003", grpc.WithInsecure())
+
+	if err != nil {
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	client := pb.NewCatClient(conn)
+	message := &pb.GetMyCatMessage{TargetCat: "tama"}
+	res, err := client.GetMyCat(context.TODO(), message)
+	fmt.Printf("result:%#v \n", res)
+	fmt.Printf("error::%#v \n", err)
+}
--- server/main.go
+++ server/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"net"
+	"os"
+
+	"com.github.Kinoshita0623/practice-grpc/app/service"
+	pb "com.github.Kinoshita0623/practice-grpc/pb/cat"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	listenPort, err := net.Listen("tcp", ":19003")
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	server := grpc.NewServer()
+	catService := &service.MyCatService{}
+	pb.RegisterCatServer(server, catService)
+	server.Serve(listenPort)
+
+}
